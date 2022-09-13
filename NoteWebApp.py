3d34de19ea645502a16725e80ba7b040bee666da
ThunderGrove <rtlund8@gmail.com>
2022-09-13 14:20:55 +0200

    End of day - create db file script completed

--- NoteWebApp.py
+++ NoteWebApp.py
@@ -1,16 +1,18 @@
 import sqlite3
 
-from flask import Flask
+from flask import Flask, request
+#from os.path
 
 app = Flask(__name__)
 
-DATABASE = '/db/dbfile.db'
+DATABASE = 'db/dbfile.db'
 
 def get_db():
     db = getattr(Flask, '_database', None)
     if db is None:
+        #if os.path.exists(DATABASE)
         db = Flask._database = sqlite3.connect(DATABASE)
-        db.execute('CREATE TABLE notes(id INT PRIMARY KEY NOT NULL,title TEXT PRIMARY KEY,note TEXT PRIMARY KEY')
+        #db.execute('CREATE TABLE notes(id INT PRIMARY KEY NOT NULL,title TEXT PRIMARY KEY,note TEXT PRIMARY KEY')
     return db
 
 @app.teardown_appcontext
@@ -19,10 +21,21 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def create_note(query):
+    cur = get_db().execute(query)
+    cur.close()
+    return "done"
+
+def get_note(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 htmlstart='<!DOCTYPE html><html><head><title>Notes in the Cloud</title></head><body>'
 htmlend='</body></html>'
 
-noteform='<form action="/TEMPACTION" method="post"><label for="title" value="">TEMPTITLE</label><input name="title" id="title"><textarea name="note" id="note"></textarea><input type="submit" value="Create note"></form>'
+noteform='<form action="/TEMPACTION" method="post"><label for="title" >TEMPTITLE</label><input name="title" id="title" value=""><textarea name="note" id="note"></textarea><input type="submit" value="Create note"></form>'
 
 @app.route("/")
 def index():
@@ -30,6 +43,16 @@ def index():
     indexform=indexform.replace('TEMPTITLE','New Note')
     return htmlstart+"<p>Hello, World!</p>"+indexform+htmlend
 
+@app.route("/note/<note_id>")
+def readnote(note_id):
+    print(get_note("select * from notes where id="+note_id+";"))
+    #indexform=noteform.replace('TEMPACTION','create')
+    #indexform=indexform.replace('TEMPTITLE','New Note')
+    return htmlstart+"<p>Hello, World!</p>"+noteform+htmlend
+
 @app.route("/create", methods=['POST', 'GET'])
 def create():
+    print(request.form)
+    if request.method == 'POST':
+        create_note('insert into notes (title,note) values("'+request.form.get('title')+'","'+request.form.get('note')+'");');
     return "<p>Hello, World!</p>"
